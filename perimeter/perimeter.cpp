ae5d2f7990968657be8575a323cc472da8f65d67
Giovanni <giosferrazza12@gmail.com>
2022-09-08 17:16:31 -0700

    first commit

---
+++ perimeter/perimeter.cpp
@@ -0,0 +1,15 @@
+#include "perimeter.h"
+
+
+
+double calcTrianglePerimeter(double side1, double side2, double side3){
+
+}
+
+double calcRectanglePerimeter(double length, double  width){
+
+}
+
+double calcCirclePerimeter(double radius){
+    return (2* radius * PI)
+}
---
+++ perimeter/perimeter.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define PI 3.1459
+
+using namespace std;
+
+
+double calcTrianglePerimeter(double a, double b, double c)
+double calcRectanglePerimeter(double, double)
+double calcCirclePerimeter(double)
